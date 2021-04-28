5f4b1f985a6a31a3e272a03dc028fcd19e3d9c5a
Your Name <you@example.com>
2021-04-28 23:20:53 +0300

    Solution fot second task

---
+++ HomeWork2/Product.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Product : Entity
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public ProductType? ProductType { get; set; }
+
+        public Product(int id, string Name, decimal Price, ProductType? productType) : base(id)
+        {
+            Id = id;
+            this.Name = Name;
+            this.Price = Price;
+            ProductType = productType;
+        }
+
+        public Product(int id) : base(id)
+        {
+
+        }
+
+        public override decimal GetFullPrice()
+        {
+            var value = base.GetFullPrice();
+            return value + 20;
+        }
+    }
+
+    public class Entity
+    {
+        public int Id { get; set; }
+
+        public Entity(int id)
+        {
+            Id = id;
+        }
+
+        public virtual decimal GetFullPrice()
+        {
+            return 10;
+        }
+    }
+
+    class Store
+    {
+        public string Name { get; set; }
+        public List<Product> assortment = new List<Product>();
+
+        public Store(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public void PrintProduct(int id)
+        {
+            Console.WriteLine($"{assortment[id].Name} | {GetValue(assortment[id].ProductType)} | {assortment[id].Price}");
+        }
+
+        public void PrintAssortment()
+        {
+            foreach (Product product in assortment)
+            {
+                Console.WriteLine($"{product.Name} | {GetValue(product.ProductType)} | {product.Price}");
+            }
+        }
+
+        public void AddProduct(Product product)
+        {
+            assortment.Add(product);
+        }
+
+        public void AddProduct(int id, string Name, decimal Price, ProductType? productType)
+        {
+            var product = new Product(id, Name, Price, productType);
+            assortment.Add(product);
+        }
+
+        string GetValue(ProductType? productType)
+        {
+            return productType switch
+            {
+                ProductType.None => "None",
+                ProductType.Mouse => "Mouse",
+                ProductType.Monitor => "Monitor",
+                ProductType.TV => "TV",
+                ProductType.Phone => "Phone",
+                ProductType.Processor => "Processor",
+                ProductType.Headphones => "Headphones",
+                ProductType.Notebook => "Notebook",
+                _ => throw new ArgumentNullException()
+            };
+        }
+    }
+
+    public abstract class PersonAbstract
+    {
+        public string Name { get; set; }
+
+        public abstract string GetFullname();
+
+        public PersonAbstract(int id)
+        {
+
+        }
+    }
+
+    public class PersonDerived : PersonAbstract
+    {
+        public PersonDerived(int id) : base(id)
+        {
+        }
+
+        public override string GetFullname()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReturnValue(ProductType productType)
+        {
+            return productType switch
+            {
+                ProductType.None => "None",
+                ProductType.Mouse => "Mouse",
+                ProductType.Monitor => "Monitor",
+                ProductType.TV => "TV",
+                ProductType.Phone => "Phone",
+                ProductType.Processor => "Processor",
+                ProductType.Headphones => "Headphones",
+                _ => throw new ArgumentNullException()
+            };
+        }
+    }
+
+    [Flags]
+    public enum ProductType
+    {
+        None,
+        Mouse,
+        Monitor,
+        TV,
+        Phone,
+        Processor,
+        Headphones,
+        Notebook
+    }
+}
---
+++ HomeWork2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product1 = new Product(0, "Razer DeathAdderEssential ", 49.23m, ProductType.Mouse);
+            var product2 = new Product(1, "Asus strix b250h gaming", 102, ProductType.Monitor);
+            var product3 = new Product(2, "Samsung Curved", 430, ProductType.TV);
+            var product4 = new Product(3, "Iphone 12", 1100, ProductType.Phone);
+            var product5 = new Product(4, "Intel core I9", 733.50m, ProductType.Processor);
+            var product6 = new Product(5, "Razer Kraken BRO", 79.99m, ProductType.Headphones);
+
+
+            var store1 = new Store("Brain");
+            store1.AddProduct(product1);
+            store1.AddProduct(product2);
+            store1.AddProduct(product3);
+            store1.AddProduct(product4);
+            store1.AddProduct(product5);
+            store1.AddProduct(product6);
+            store1.AddProduct(6, "Xiaomi notebook", 300, ProductType.Notebook);
+            store1.PrintAssortment();
+
+            var client1 = new Client(1, "Eugene Rudenberg");
+
+            var order1 = new Order(store1, client1);
+            order1.AddProduct(store1.assortment[2]);
+            order1.AddProduct(store1.assortment[3]);
+            order1.GetProducts();
+
+            Console.WriteLine(order1.GetOrderPrice());
+
+            Console.ReadLine();
+        }
+
+
+        public record ProductRecord(int Id);
+    }
+}
