829840fb67e977a048fa9aae650ee3e9cacf8016
Your Name <you@example.com>
2021-05-17 20:27:45 +0300

    Fixed some methods and implemented business logic

---
+++ HomeWork2/PersonAbstract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    public abstract class PersonAbstract
+    {
+        public string Name { get; set; }
+
+        public abstract string GetFullname();
+
+    }
+}
